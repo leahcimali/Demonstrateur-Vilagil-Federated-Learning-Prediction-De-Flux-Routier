f9ad5652cc93c2a081285f0a3418f89f02074820
Michael <michaelbenalipro@gmail.com>
2023-05-26 14:41:37 +0200

    Added a test.py and export results in a txt file The results of main.py and test.py are exported in txt file The results of test.py will be exported as json Results are updated in real time.

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-
+from os import makedirs
 import torch
+import importlib
+import contextlib
 
 from src.utils_data import load_PeMS04_flow_data, preprocess_PeMS_data, local_dataset, plot_prediction
-from src.models import LSTMModel
 from src.utils_training import train_model, testmodel
 from src.utils_fed import fed_training_plan
 from src.metrics import calculate_metrics, metrics_table
 import src.config
 import sys
 
-import contextlib
 import json 
 
 seed = 42
@@ -24,14 +24,22 @@ config_file_path = sys.argv[1]
 
 params = src.config.Params(config_file_path)
 
-with open(params.model_path +'test.txt', 'w') as f:
-    with contextlib.redirect_stdout(f):
+path_folder = params.save_model_path.split("/")[:-1]
+new_path = '/'.join(path_folder)
+makedirs(new_path, exist_ok=True)
 
-        LSTM_input_size = 1
-        LSTM_hidden_size = 32
-        LSTM_num_layers = 6
-        LSTM_output_size = 1
+with open(params.save_model_path +'test.txt', 'w') as f:
+    with contextlib.redirect_stdout(src.config.Tee(f, sys.stdout)):
 
+        module_name = 'src.models'
+        class_name = params.model
+        module = importlib.import_module(module_name)
+        model = getattr(module, class_name)
+
+        input_size = 1
+        hidden_size = 32
+        num_layers = 6
+        output_size = 1
 
         #Load traffic flow dataframe and graph dataframe from PEMS
         df_PeMS, distance = load_PeMS04_flow_data()
@@ -48,17 +56,17 @@ with open(params.model_path +'test.txt', 'w') as f:
         metrics_dict ={}
         for node in range(len(params.nodes_to_filter)):
             metrics_dict[node]={}
-            y_true, y_pred = testmodel(LSTMModel(1,32,1), datadict[node]['val'], f'{params.model_path}local{node}.pth', meanstd_dict = meanstd_dict, sensor_order_list=[params.nodes_to_filter[node]])  
+            y_true, y_pred = testmodel(model(1,32,1), datadict[node]['val'], f'{params.save_model_path}local{node}.pth', meanstd_dict = meanstd_dict, sensor_order_list=[params.nodes_to_filter[node]])  
             local_metrics = calculate_metrics(y_true, y_pred)
             metrics_dict[node]['local_only'] = local_metrics
             for round in range(1, params.communication_rounds+1):
-                y_true_fed, y_pred_fed = testmodel(LSTMModel(1,32,1), datadict[node]['val'], f'{params.model_path}model_round_{round}.pth', meanstd_dict = meanstd_dict, sensor_order_list=[params.nodes_to_filter[node]])
+                y_true_fed, y_pred_fed = testmodel(model(1,32,1), datadict[node]['val'], f'{params.save_model_path}model_round_{round}.pth', meanstd_dict = meanstd_dict, sensor_order_list=[params.nodes_to_filter[node]])
                 fed_metric = calculate_metrics(y_true_fed, y_pred_fed)
                 metrics_dict[node][f'fed_round_{round}'] = fed_metric
                 print(f'Federated vs local only for node {node} :')
                 print(metrics_table({'Local' :local_metrics, f'Federated Round {round}' : fed_metric }))
 
-with open(params.model_path +"test.json", "w") as outfile:
+with open(params.save_model_path + "test.json", "w") as outfile:
     json.dump(metrics_dict, outfile)
 
 
