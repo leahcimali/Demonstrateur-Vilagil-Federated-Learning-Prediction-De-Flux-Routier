86ed7c2fb2acbd8b66fb65005db26194ed300dff
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-06-30 16:46:41 +0200

    Refactor_5: Split the main function into smaller functions
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- src/sub_pages_one_sensor/single_sensor_map.py
+++ src/sub_pages_one_sensor/single_sensor_map.py
@@ -1,9 +1,5 @@
-from os import path
-
-
 import streamlit as st
 from streamlit_folium import folium_static
-import json
 import pandas as pd
 import folium
 from screeninfo import get_monitors
@@ -49,9 +45,15 @@ WIDTH = min(width)
 #######################################################################
 # Function(s)
 #######################################################################
-def plot_map(experiment_path, mapping_sensor_with_nodes, sensor_selected):
-    annotated_text(
-        "A higher percent indicates a better prediction. So, ",
+def load_experiment_results(experiment_path, sensor_selected):
+    y_true = load_numpy(f"{experiment_path}/y_true_local_{sensor_selected}.npy")
+    y_pred_local = load_numpy(f"{experiment_path}/y_pred_local_{sensor_selected}.npy")
+    y_pred_fed = load_numpy(f"{experiment_path}/y_pred_fed_{sensor_selected}.npy")
+    return y_true, y_pred_local, y_pred_fed
+
+
+def text_introduction_map():
+    annotated_text("A higher percent indicates a better prediction. So, ",
         ("green", "", "#75ff5b"), " circle",
         " is better than the ",
         ("red", "", "#fe7597"), " one.")
@@ -62,49 +64,56 @@ def plot_map(experiment_path, mapping_sensor_with_nodes, sensor_selected):
             """)
     st.divider()
 
+
+def render_map(seattle_map_fed, seattle_map_local):
+    seattle_map_fed.fit_bounds(seattle_map_fed.get_bounds(), padding=(90, 90))
+    seattle_map_local.fit_bounds(seattle_map_local.get_bounds(), padding=(90, 90))
+
+    st.header("MAP")
+    col1, col2 = st.columns((0.5, 0.5), gap="small")
+    with col1:
+        col1.header('Federated model results')
+        folium_static(seattle_map_fed, width=WIDTH / 2 - 300)  # To fix the overlapping effect (handmade solution)
+    with col2:
+        col2.header('Local models results')
+        folium_static(seattle_map_local, width=WIDTH / 2 - 300)  # To fix the overlapping effect (handmade solution)
+
+
+def add_metric_on_map(experiment_path, sensor_selected, coords, seattle_map_fed, seattle_map_local, i):
+    y_true, y_pred_local, y_pred_fed = load_experiment_results(experiment_path, sensor_selected)
+    maape_computed_local = 1 - (maape(y_true[i, :].flatten(), y_pred_local[i, :].flatten())) / 100.0
+    maape_computed_fed = 1 - (maape(y_true[i, :].flatten(), y_pred_fed[i, :].flatten())) / 100.0
+    color_fed, color_local = get_color_fed_vs_local(maape_computed_fed, maape_computed_local)
+
+    create_circle_precision_predict(coords, maape_computed_local, seattle_map_local, color_local)
+    create_circle_precision_predict(coords, maape_computed_fed, seattle_map_fed, color_fed)
+
+
+def create_map(experiment_path, sensor_selected, params, i):
+    sensor_loc = SEATTLE_ROADS[0]
+    seattle_map_fed = folium.Map(location=sensor_loc, zoom_start=15, zoomSnap=0.25)
+    seattle_map_local = folium.Map(location=sensor_loc, zoom_start=15, zoomSnap=0.25)
+
+    folium.Marker(location=sensor_loc, tooltip=params.nodes_to_filter[int(sensor_selected)], icon=folium.Icon(color="black")).add_to(seattle_map_fed)
+    folium.Marker(location=sensor_loc, tooltip=params.nodes_to_filter[int(sensor_selected)], icon=folium.Icon(color="black")).add_to(seattle_map_local)
+
+    add_metric_on_map(experiment_path, sensor_selected, sensor_loc, seattle_map_fed, seattle_map_local, i)
+    return seattle_map_fed, seattle_map_local
+
+
+def plot_map(experiment_path, sensor_selected):
+    text_introduction_map()
+
     params = Params(f'{experiment_path}/config.json')
     index = load_numpy(f"{experiment_path}/index_0.npy")
     index = pd.to_datetime(index, format='%Y-%m-%dT%H:%M:%S.%f')
 
     slider = st.slider('Select the step (a step equal 5min)?', 0, len(index) - params.prediction_horizon - params.window_size - 1, 0, key="MAP_and_Graph")
 
-    sensors_loc = {}
-    seattle_roads_crop = [SEATTLE_ROADS[i] for i in range(len(mapping_sensor_with_nodes.keys()))]
-
-    for sensor, locations in zip(mapping_sensor_with_nodes.keys(), seattle_roads_crop):
-        sensors_loc[sensor] = locations
-
-    seattle_map_global = folium.Map(location=sensors_loc[params.nodes_to_filter[int(sensor_selected)]], zoom_start=15, zoomSnap=0.25)
-    seattle_map_local = folium.Map(location=sensors_loc[params.nodes_to_filter[int(sensor_selected)]], zoom_start=15, zoomSnap=0.25)
-
-    folium.Marker(location=sensors_loc[params.nodes_to_filter[int(sensor_selected)]], tooltip=params.nodes_to_filter[int(sensor_selected)], icon=folium.Icon(color="black")).add_to(seattle_map_global)
-    folium.Marker(location=sensors_loc[params.nodes_to_filter[int(sensor_selected)]], tooltip=params.nodes_to_filter[int(sensor_selected)], icon=folium.Icon(color="black")).add_to(seattle_map_local)
-
-    def plot_map_slider(y_true, y_pred, y_pred_fed, i, coords):
-
-        maape_computed_local = 1 - (maape(y_true[i, :].flatten(), y_pred[i, :].flatten())) / 100.0
-        maape_computed_fed = 1 - (maape(y_true[i, :].flatten(), y_pred_fed[i, :].flatten())) / 100.0
-        color_fed, color_local = get_color_fed_vs_local(maape_computed_fed, maape_computed_local)
-
-        create_circle_precision_predict(coords, maape_computed_local, seattle_map_local, color_local)
-        create_circle_precision_predict(coords, maape_computed_fed, seattle_map_global, color_fed)
-
-    y_true = load_numpy(f"{experiment_path}/y_true_local_{sensor_selected}.npy")
-    y_pred = load_numpy(f"{experiment_path}/y_pred_local_{sensor_selected}.npy")
-    y_pred_fed = load_numpy(f"{experiment_path}/y_pred_fed_{sensor_selected}.npy")
-    plot_map_slider(y_true, y_pred, y_pred_fed, slider, sensors_loc[params.nodes_to_filter[int(sensor_selected)]])
-
-    seattle_map_global.fit_bounds(seattle_map_global.get_bounds(), padding=(90, 90))
-    seattle_map_local.fit_bounds(seattle_map_local.get_bounds(), padding=(90, 90))
+    seattle_map_fed, seattle_map_local = create_map(experiment_path, sensor_selected, params, slider)
 
     st.header(f"| {index[slider+params.window_size].strftime(f'Day: %Y-%m-%d | Time prediction: {int(params.prediction_horizon*5/60)}h (%Hh%Mmin')} to {index[slider + params.window_size + params.prediction_horizon].strftime(f'%Hh%Mmin) | Step : {slider} |')}")
-    col1, col2 = st.columns((0.5, 0.5), gap="small")
-    with col1:
-        col1.header('Federated model results')
-        folium_static(seattle_map_global, width=WIDTH / 2 - 300)  # To fix the overlapping effect (handmade solution)
-    with col2:
-        col2.header('Local models results')
-        folium_static(seattle_map_local, width=WIDTH / 2 - 300)  # To fix the overlapping effect (handmade solution)
+    render_map(seattle_map_fed, seattle_map_local)
 
 
 #######################################################################
@@ -120,16 +129,4 @@ def single_sensor_map_sensor(experiment_path, sensor_selected):
 
     path_experiment_selected = experiment_path
     if (path_experiment_selected is not None):
-
-        with open(f"{path_experiment_selected}/test.json") as f:
-            results = json.load(f)
-        with open(f"{path_experiment_selected}/config.json") as f:
-            config = json.load(f)
-
-        mapping_sensor_with_node = {}
-        for node in results.keys():  # e.g. keys = ['0', '1', '2', ...]
-            mapping_sensor_with_node[config["nodes_to_filter"][int(node)]] = node  # e.g. nodes_to_filter = [118, 261, 10, ...]
-
-        if (path.exists(f'{path_experiment_selected}/y_true_local_0.npy') and
-            path.exists(f"{path_experiment_selected}/y_pred_fed_0.npy")):
-            plot_map(path_experiment_selected, mapping_sensor_with_node, sensor_selected)
+        plot_map(path_experiment_selected, sensor_selected)
