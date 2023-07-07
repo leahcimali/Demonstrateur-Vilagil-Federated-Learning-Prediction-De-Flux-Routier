8b7a833d10020dfb887072559f2ba9fa232c92d9
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-07-07 16:49:17 +0200

    add: two sorting options to 4_Cluster.py
    * Sort by Local or Federated values
    * Sort in ascending or descending order
    
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- src/pages/4_Cluster.py
+++ src/pages/4_Cluster.py
@@ -25,16 +25,28 @@ class Cluster:
         super(Cluster, self).__init__()
         self.cluster = cluster
         self.parameters = config_cluster
-        self.nodes = cluster.keys()
-        self.sensors = config_cluster["nodes_to_filter"]
+        self.sensors = cluster.keys()
+        self.sensors_name = config_cluster["nodes_to_filter"]
         self.name = config_cluster["save_model_path"]
         self.size = len(cluster)
 
-    def get_node_metric_local(self, node, metric):
+    def get_sensor_metric_local_values(self, node, metric):
         return self.cluster[node]["local_only"][metric]
 
-    def get_node_metric_federated(self, node, metric):
+    def get_sensor_metric_federated_values(self, node, metric):
         return self.cluster[node]["Federated"][metric]
+
+    def get_nb_sensor_better_in_federation(self, metric):
+        nb_sensor = 0
+        if metric != "Superior Pred %":
+            for sensor in self.sensors:
+                if self.cluster[sensor]["Federated"][metric] <= self.cluster[sensor]["local_only"][metric]:
+                    nb_sensor += 1
+        else:
+            for sensor in self.sensors:
+                if self.cluster[sensor]["Federated"][metric] >= self.cluster[sensor]["local_only"][metric]:
+                    nb_sensor += 1
+        return nb_sensor
 
     def show_parameters(self):
         # Création du DataFrame
@@ -66,7 +78,8 @@ class Cluster:
         }
 
         # Affichage du tableau récapitulatif
-        st.write("**Parameters of clusters**")
+        st.subheader("Parameters of the cluster")
+        st.write("Note: only the number of sensor and the sensors use in the cluster change between clusters.")
         st.write("")
         st.write("WS (**Windows size**), how many steps use to make a prediction")
         st.write("PH (**Prediction horizon**), how far the prediction goes (how many steps)")
@@ -76,16 +89,24 @@ class Cluster:
         st.dataframe(df_parameters, use_container_width=True)
 
 
-def render_bar_plot_fed_vs_local(cluster, metric, descending=False):
-    value_metric_local = []
-    value_metric_federated = []
-    for node in cluster.nodes:
-        value_metric_local.append(cluster.get_node_metric_local(node, metric))
-        value_metric_federated.append(cluster.get_node_metric_federated(node, metric))
+def render_bar_plot_fed_vs_local(cluster, metric, sorted_by: str, descending: str):
+    st.subheader(f"Comparison between the federated version and local version with the {metric} metric on each sensors")
 
-    sorted_value_metric_federated = sorted(value_metric_federated, reverse=descending)
-    sorted_value_metric_local = [value for _, value in sorted(zip(value_metric_federated, value_metric_local))]
-    sorted_sensor_name = [str(name) for _, name in sorted(zip(value_metric_federated, cluster.sensors))]
+    value_metric_federated = []
+    value_metric_local = []
+    for sensor in cluster.sensors:
+        value_metric_local.append(cluster.get_sensor_metric_local_values(sensor, metric))
+        value_metric_federated.append(cluster.get_sensor_metric_federated_values(sensor, metric))
+
+    descending = descending == "Descending"
+    if sorted_by == "Federated":
+        sorted_value_metric_federated = sorted(value_metric_federated, reverse=descending)
+        sorted_value_metric_local = [value for _, value in sorted(zip(value_metric_federated, value_metric_local), reverse=descending)]
+    else:
+        sorted_value_metric_local = sorted(value_metric_local, reverse=descending)
+        sorted_value_metric_federated = [value for _, value in sorted(zip(value_metric_local, value_metric_federated), reverse=descending)]
+
+    sorted_sensor_name = [str(name) for _, name in sorted(zip(value_metric_federated, cluster.sensors_name))]
 
     max_value = max(sorted_value_metric_federated, sorted_value_metric_local)
 
@@ -96,53 +117,44 @@ def render_bar_plot_fed_vs_local(cluster, metric, descending=False):
         bar_trace_federated = go.Bar(
             x=[f"sensor: {sensor}"],
             y=[federated_value],
-            name=f'{sensor} Federated {metric} = {federated_value}',
+            name=f'{sensor} Federated | {metric} = {federated_value}',
             orientation="v",
-            marker_color="rgb(120, 120, 120)",
+            marker_color="rgb(50, 50, 64)",
+            legendgroup=sensor,
             width=0.2,
-            offset=-0.12
+            offset=-0.13
         )
         bar_trace_local = go.Bar(
             x=[f"sensor: {sensor}"],
             y=[local_value],
-            name=f'{sensor} Local {metric} = {local_value}',
+            name=f'{sensor} Local | {metric} = {local_value}',
             orientation="v",
-            marker_color="rgb(220, 120, 220)",
+            marker_color="rgb(240, 100, 200)",
+            legendgroup=sensor,
             width=0.2,
-            offset=0.12
+            offset=0.13
         )
         fig.add_trace(bar_trace_federated)
         fig.add_trace(bar_trace_local)
 
-    # Ajouter un thème sombre au graphique
-    fig.update_layout(template="plotly_dark")
-
-    # Ajouter un titre au graphique
-    fig.update_layout(title=f"Comparison between federated version and local version on the {metric} metric")
-
-    # Ajouter des légendes aux axes x et y
     fig.update_xaxes(title="Sensors name")
     fig.update_yaxes(title=f"{metric} value", range=[0, max_value], dtick=5)
 
     fig.update_layout(
+        template="plotly_dark",
         showlegend=True,
         height=800,
         margin=dict(l=0, r=0, t=30, b=0),
         xaxis_tickangle=45,
+        hovermode="x unified"
     )
-    fig.update_layout(hovermode="x unified")
 
-    nb_sensors = sum(
-        federated_value <= local_value
-        for federated_value, local_value in zip(
-            value_metric_federated, value_metric_local
-        )
-    )
+    nb_sensors = cluster.get_nb_sensor_better_in_federation(metric)
+
     fig.add_annotation(
         x=0,
         y=1,
-        text=f"{round(nb_sensors / len(value_metric_federated)*100, 2)}% ({nb_sensors}/{len(value_metric_federated)}) " +
-        "sensors have better results with the federated approach",
+        text=f"{round(nb_sensors / cluster.size * 100, 2)}% ({nb_sensors}/{cluster.size}) sensors have better results with the federated approach",
         font=dict(size=18, color="black"),
         showarrow=False,
         xref="paper",
@@ -150,7 +162,7 @@ def render_bar_plot_fed_vs_local(cluster, metric, descending=False):
         xanchor="left",
         yanchor="top",
         bordercolor="red",
-        borderwidth=5,
+        borderwidth=3,
         bgcolor="white",
     )
 
@@ -160,9 +172,9 @@ def render_bar_plot_fed_vs_local(cluster, metric, descending=False):
 #######################################################################
 # Main
 #######################################################################
-st.subheader("Comparison Clusters")
+st.subheader("Visiualization of one cluster")
 st.write("""
-        * TODO
+        * Visualization of one cluster
         """)
 st.divider()
 
@@ -176,6 +188,13 @@ for path_exp in experiments_path:
     cluster = Cluster(load_experiment_results(path_exp_parent), load_experiment_config(path_exp_parent))
     clusters[cluster.name.split("/")[1]] = cluster
 
-cluster = st.selectbox("Select the cluster", [name for name in clusters])
+cluster = st.selectbox("Select the cluster", list(clusters))
 clusters[cluster].show_parameters()
-render_bar_plot_fed_vs_local(clusters[cluster], "RMSE")
+
+metric = st.selectbox("Choose the metric", METRICS)
+col1, col2 = st.columns(2)
+with col1:
+    descending = st.radio("Sorted:", ["Descending", "Ascending"], index=1)
+with col2:
+    sorted_by = st.radio("Sorted_by:", ["Federated", "Local"], index=1)
+render_bar_plot_fed_vs_local(clusters[cluster], metric, sorted_by, descending=descending)
