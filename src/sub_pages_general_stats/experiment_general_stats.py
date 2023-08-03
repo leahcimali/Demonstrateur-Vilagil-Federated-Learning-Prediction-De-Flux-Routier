5afb1a4f2acb44dbe5e41545823d0f43bf209f5c
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-08-03 09:41:38 +0200

    add avg_rate_change
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- src/sub_pages_general_stats/experiment_general_stats.py
+++ src/sub_pages_general_stats/experiment_general_stats.py
@@ -3,6 +3,7 @@
 ###############################################################################
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 from utils_streamlit_app import selection_of_experiment, style_dataframe, load_experiment_results, results_to_stats_dataframe, get_colors_for_results, get_results_for_key, get_name_version_normalized
 
@@ -25,6 +26,7 @@ def render_results(df_fed, df_local):
 
 
 def render_experiment(path_experiment_selected):
+    st.write(path_experiment_selected)
     results = load_experiment_results(path_experiment_selected)
     sensor_index = results.keys()  # e.g. keys = ['0', '1', '2', ...]
     st.subheader(f"A comparison between federated and local version | Average on {len(sensor_index)} sensors")
@@ -42,10 +44,19 @@ def render_experiment(path_experiment_selected):
 
     st.markdown(f"There is :red[**{nb_sensor_better_in_federation} sensors**] on {len(results_sensor_federated)} improved by the federation")
 
+    metrics = ["RMSE", "MAE", "MAAPE", "Superior Pred %"]
+    avg_rate_change = {}
+    for metric in metrics:
+        for i in range(len(results_sensor_federated)):
+            if metric not in avg_rate_change.keys():
+                avg_rate_change[metric] = 1 + ((results_sensor_federated[i][metric] - results_sensor_local[i][metric]) / results_sensor_local[i][metric])
+            else:
+                avg_rate_change[metric] = avg_rate_change[metric] * (1 + ((results_sensor_federated[i][metric] - results_sensor_local[i][metric]) / results_sensor_local[i][metric]))
+    for metric in metrics:
+        avg_rate_change[metric] = (np.power(avg_rate_change[metric], (1/len(results_sensor_federated))) - 1) * 100
     if results_sensor_federated and results_sensor_local:
         stats_fed_ver = results_to_stats_dataframe(results_sensor_federated)
         stats_local_ver = results_to_stats_dataframe(results_sensor_local)
-
     mean_stats_fed_ver = stats_fed_ver["mean"]
     mean_stats_local_ver = stats_local_ver["mean"]
 
@@ -60,6 +71,9 @@ def render_experiment(path_experiment_selected):
     df_mean_diff = df_mean_diff.applymap(lambda x: '{:.2f}'.format(x))
     st.table(df_mean_diff.style.set_table_styles(style_dataframe(df_mean_diff, colors="#000000", column_index=2)))
 
+    avg_rate_change = pd.DataFrame.from_dict(avg_rate_change, orient="index", columns=["Average rate of change"])
+    avg_rate_change = avg_rate_change.applymap(lambda x: '{:.2f} %'.format(x))
+    st.table(avg_rate_change.style.set_table_styles(style_dataframe(avg_rate_change, colors="#000000", column_index=2)))
 
 #######################################################################
 # Main
