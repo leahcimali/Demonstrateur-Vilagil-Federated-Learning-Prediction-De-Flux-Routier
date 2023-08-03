cd9dd41ad86c1a9828dad95cf35efd2c738e1e32
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-08-03 16:16:28 +0200

    [WIP] animation traffic flow
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- animation_test.py
+++ animation_test.py
@@ -5,23 +5,22 @@ sys.path.append("./src/")
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QWheelEvent
 from PyQt5.QtCore import Qt
-import plotly.graph_objects as go
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsProxyWidget, QSizePolicy, QSlider
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsProxyWidget, QSizePolicy, QSlider, QPushButton
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-
 from pathlib import PurePath
 from metrics import rmse
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 import networkx as nx
+import pandas as pd
 
 from src.ClusterData import ClusterData
 from src.utils_data import load_PeMS04_flow_data
 from src.utils_graph import create_graph
 from src.utils_streamlit_app import load_experiment_results, load_numpy, get_color_fed_vs_local, load_experiment_config
-import pandas as pd
 
 
 def load_experiment_results_sensor(experiment_path, sensor_selected):
@@ -34,16 +33,6 @@ def load_experiment_results_sensor(experiment_path, sensor_selected):
 #######################################################################
     # Figure(s)
 #######################################################################
-def add_y_true(fig, df):
-    fig.add_scatter(
-        x=df['Time'],
-        y=df['y_true'],
-        mode='lines',
-        marker=dict(color='blue'),
-        name='y_true'
-    )
-
-
 def add_y_pred_federation(fig, df, color_fed, rmse_fed):
     fig.add_scatter(
         x=df['Time'],
@@ -92,36 +81,23 @@ def update_yaxis_range(fig, range_yaxis):
     )
 
 
-def add_vrectangle(fig, x0, x1):
-    fig.add_vrect(
-        x0=x0,
-        x1=x1,
-        fillcolor='gray',
-        opacity=0.2,
-        line_width=0
-    )
-
-
 def update_xaxis_range(fig, range_xaxis):
     fig.update_xaxes(
         title='Time',
         tickformat='%H:%M',
         range=range_xaxis,
-        dtick=3600000
+        dtick=300000
     )
 
 
 def create_fig(df, color_fed, color_local, rmse_fed, rmse_local):
-    fig = px.line(
-        df, x='Time', y=["Window"], color_discrete_sequence=['black']
-    )
-    add_y_true(fig, df)
+    fig = go.Figure()
 
     add_y_pred_federation(fig, df, color_fed, rmse_fed)
-    add_y_pred_federation_link_window(fig, df, color_fed)
+    # add_y_pred_federation_link_window(fig, df, color_fed)
 
-    add_y_pred_local(fig, df, color_local, rmse_local)
-    add_y_pred_local_link_window(fig, df, color_local)
+    # add_y_pred_local(fig, df, color_local, rmse_local)
+    # add_y_pred_local_link_window(fig, df, color_local)
     return fig
 
 
@@ -130,7 +106,7 @@ def update_axis(fig, range_x_axis, range_y_axis):
     update_yaxis_range(fig, range_y_axis)
 
 
-def update_layout_fig(fig, config, index, i):
+def update_layout_fig(fig):
     fig.update_layout(
         title="",
         title_font=dict(size=25),
@@ -141,33 +117,20 @@ def update_layout_fig(fig, config, index, i):
             xanchor='right'
         )
     )
-    add_vrectangle(fig, index[i], index[i + config["window_size"] - 1])
 
 
 #######################################################################
     # Dataframe
 #######################################################################
 def add_y_pred_fed_to_df(df, config, y_pred_fed):
-    df['y_pred_federation'] = np.concatenate([np.repeat(np.nan, config["window_size"]).reshape(-1, 1), y_pred_fed])
-    df["y_pred_federation_link_window"] = np.concatenate([np.repeat(np.nan, config["window_size"]).reshape(-1, 1), y_pred_fed])
-    df["y_pred_federation_link_window"].at[config["window_size"] - 1] = df['Window'].iloc[config["window_size"] - 1]
-    return df
-
-
-def add_y_pred_local_to_df(df, config, y_pred_local):
-    df['y_pred_local'] = np.concatenate([np.repeat(np.nan, config["window_size"]).reshape(-1, 1), y_pred_local])
-    df["y_pred_local_link_window"] = np.concatenate([np.repeat(np.nan, config["window_size"]).reshape(-1, 1), y_pred_local])
-    df["y_pred_local_link_window"].at[config["window_size"] - 1] = df['Window'].iloc[config["window_size"] - 1]
+    df['y_pred_federation'] = y_pred_fed
     return df
 
 
 def create_dataframe(index, test_set, config, y_pred_fed, y_pred_local, i):
-    df = pd.DataFrame({'Time': index[i:i + config["window_size"] + config["prediction_horizon"]], 'Traffic Flow': test_set[i:i + config["window_size"] + config["prediction_horizon"]].flatten()})
-    df['Window'] = df['Traffic Flow'].where((df['Time'] >= index[i]) & (df['Time'] < index[i + config["window_size"]]))
-    df['y_true'] = df['Traffic Flow'].where((df['Time'] >= index[i + config["window_size"] - 1]))
+    df = pd.DataFrame({'Time': index[i + config["window_size"]: i + config["window_size"] + config["prediction_horizon"]], 'Traffic Flow': test_set[i + config["window_size"]: i + config["window_size"] + config["prediction_horizon"]].flatten()})
 
     df = add_y_pred_fed_to_df(df, config, y_pred_fed)
-    df = add_y_pred_local_to_df(df, config, y_pred_local)
     return df
 
 
@@ -177,7 +140,7 @@ def graph_prediction(experiment_path, index, config, sensor_selected, i=0):
     rmse_fed = rmse(y_true[i, :].flatten(), y_pred_fed[i, :].flatten())
     rmse_local = rmse(y_true[i, :].flatten(), y_pred_local[i, :].flatten())
 
-    color_fed, color_local = get_color_fed_vs_local(rmse_fed, rmse_local, superior=False)
+    color_fed, color_local = ("#00dd00", "#fe4269")
 
     df = create_dataframe(index, test_set, config, y_pred_fed[i, :], y_pred_local[i, :], i)
 
@@ -185,10 +148,10 @@ def graph_prediction(experiment_path, index, config, sensor_selected, i=0):
 
     min_yaxis = min(min(y_true.flatten()), min((min(y_pred_fed.flatten()), min(y_pred_local.flatten()))))
     max_yaxis = max((max(y_true.flatten()), max(y_pred_fed.flatten()), max(y_pred_local.flatten())))
-    update_axis(fig, [index[i], index[i + config["window_size"] + config["prediction_horizon"]]],
+    update_axis(fig, [index[i + config["window_size"]], index[i + config["window_size"] + config["prediction_horizon"]]],
                 [min_yaxis, max_yaxis])
 
-    update_layout_fig(fig, config, index, i)
+    update_layout_fig(fig)
     return fig
 
 
@@ -210,8 +173,9 @@ class ZoomableWebEngineView(QWebEngineView):
 class MainWindow(QMainWindow):
     def __init__(self, G, cluster):
         super().__init__()
-        index = load_numpy(f"{cluster.path_to_exp}/index_{cluster.indexes[0]}.npy")
-        index = pd.to_datetime(index, format='%Y-%m-%dT%H:%M:%S.%f')
+        self.cluster = cluster
+        self.index = load_numpy(f"{cluster.path_to_exp}/index_{cluster.indexes[0]}.npy")
+        self.index = pd.to_datetime(self.index, format='%Y-%m-%dT%H:%M:%S.%f')
 
         # Create QGraphicsView to hold the plot widgets
         self.plot_view = QGraphicsView(self)
@@ -222,26 +186,35 @@ class MainWindow(QMainWindow):
         self.plot_scene = QGraphicsScene(self)
         self.plot_scene.setSceneRect(0, 0, 2450, 650)  # Prendre une plus grande taille pour accueillir tous les graphiques (widget)
 
-        slider = QSlider(Qt.Horizontal)
-        slider.setFocusPolicy(Qt.StrongFocus)
-        slider.setTickPosition(QSlider.TicksBothSides)
-        slider.setTickInterval(len(index))
-        slider.setSingleStep(1)
-        slider.valueChanged.connect(self.update)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setFocusPolicy(Qt.StrongFocus)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.setTickInterval(len(self.index))
+        self.slider.setSingleStep(1)
+        self.slider.valueChanged.connect(self.update_slider)
 
-        i = 0
+        widget_in_scene = self.plot_scene.addWidget(self.slider)
+        widget_in_scene.setPos(50, 650)
+
+        self.button = QPushButton("Start Slider")
+        self.button.clicked.connect(self.toggle_slider)
+        widget_in_scene = self.plot_scene.addWidget(self.button)
+        widget_in_scene.setPos(50, 700)
+
+        self.slider_timer = None
+        self.slider_direction = 1
+
+        self.map_sensor_graph = {}
+        self.map_sensor_webview = {}
         for sensor in ['0', '1', '2']:
-            self.plot_widget = QWebEngineView()
-            figure = graph_prediction(cluster.path_to_exp, index, cluster.parameters, sensor)
-            self.plot_widget.setHtml(figure.to_html(include_plotlyjs='cdn'))
-            self.plot_widget.setContentsMargins(0, 0, 0, 0)  # Remove margins and padding
-            self.plot_widget.setFixedSize(800, 600)  # Adjust the size of the first widget
-            self.plot_proxy1 = self.plot_scene.addWidget(self.plot_widget)
-            self.plot_proxy1.setPos(805 * i, 0)  # Set the position of the first widget within the scene
-            i = i + 1
+            self.map_sensor_webview[sensor] = QWebEngineView()
+            self.map_sensor_graph[sensor] = graph_prediction(cluster.path_to_exp, self.index, cluster.parameters, sensor, 0)
+            self.map_sensor_webview[sensor].setHtml(self.map_sensor_graph[sensor].to_html(include_plotlyjs='cdn'))
+            self.map_sensor_webview[sensor].setContentsMargins(0, 0, 0, 0)
+            self.map_sensor_webview[sensor].setFixedSize(800, 600)
+            widget_in_scene = self.plot_scene.addWidget(self.map_sensor_webview[sensor])
+            widget_in_scene.setPos(805 * int(sensor), 0)
 
-        self.plot_proxy1 = self.plot_scene.addWidget(slider)
-        self.plot_proxy1.setPos(50, 650)
         # Add the QGraphicsScene to the QGraphicsView
         self.plot_view.setScene(self.plot_scene)
 
@@ -269,8 +242,44 @@ class MainWindow(QMainWindow):
         else:
             super().wheelEvent(event)
 
-    def update(self, value):
-        print(value)
+    def update_slider(self, value):
+        self.plot_scene.setSceneRect(0, 0, 2450, 650)  # Prendre une plus grande taille pour accueillir tous les graphiques (widget)
+        for sensor in ['0', '1', '2']:
+            y_true, y_pred_local, y_pred_fed, test_set = load_experiment_results_sensor(self.cluster.path_to_exp, sensor)
+            df = create_dataframe(self.index, test_set, self.cluster.parameters, y_pred_fed[value, :], y_pred_local[value, :], value)
+            self.map_sensor_graph[sensor].update_traces(x=df["Time"], y=df["y_pred_federation"])
+            min_yaxis = min(min(y_true.flatten()), min((min(y_pred_fed.flatten()), min(y_pred_local.flatten()))))
+            max_yaxis = max((max(y_true.flatten()), max(y_pred_fed.flatten()), max(y_pred_local.flatten())))
+            update_axis(self.map_sensor_graph[sensor], [self.index[value + self.cluster.parameters["window_size"]], self.index[value + self.cluster.parameters["window_size"] + self.cluster.parameters["prediction_horizon"]]],
+                [min_yaxis, max_yaxis])
+            self.map_sensor_webview[sensor].setHtml(self.map_sensor_graph[sensor].to_html(include_plotlyjs='cdn'))
+            #self.map_sensor_webview[sensor].reload()
+            widget_in_scene = self.plot_scene.addWidget(self.map_sensor_webview[sensor])
+            widget_in_scene.setPos(805 * int(sensor), 0)
+
+    def toggle_slider(self):
+        if self.slider_timer is None:
+            self.slider_timer = self.startTimer(500)  # Adjust the timer delay as needed
+            self.button.setText("Stop Slider")
+        else:
+            self.killTimer(self.slider_timer)
+            self.slider_timer = None
+            self.button.setText("Start Slider")
+
+    def timerEvent(self, event):
+        current_value = self.slider.value()
+        if self.slider_direction == 1:
+            if current_value < self.slider.maximum():
+                self.slider.setValue(current_value + 1)
+            else:
+                self.slider_direction = -1
+        else:
+            if current_value > self.slider.minimum():
+                self.slider.setValue(current_value - 1)
+            else:
+                self.slider_direction = 1
+
+
 if __name__ == '__main__':
     path_exp = "community_experiments/28_cluster_with_Louvain_Algo/community0/"
     clusters = {}
